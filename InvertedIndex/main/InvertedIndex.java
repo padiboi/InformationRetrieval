c2a94b02ece08a03685a6e25381f2e5fb3b418fa
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-01-15 20:37:20 +0530

    Add term normalizer
    Includes case-folding to lowercase and removal of stop words.
    
    Update .gitignore
    
    TBS commit

--- InvertedIndex/main/InvertedIndex.java
+++ InvertedIndex/main/InvertedIndex.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package InvertedIndex;
+
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
@@ -10,6 +12,7 @@ import java.io.IOException;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
 /**
  *
  * @author gautam
@@ -28,17 +31,16 @@ public class InvertedIndex {
     }              
     
     private void constructDictionary(TreeMap dict) {
-        File folder = new File("../preprocessing/data/");
+        File folder = new File("../../preprocessing/data/");
         File[] listOfFiles = folder.listFiles();
         
         int i = 0;
         for (File file : listOfFiles) {
             if (file.isFile()) {
                 docs.put(file.getName(), i++);
-                System.out.println(file.getName());
                 // tokenize and add to dict
                 try {
-                    String content = new String(Files.readAllBytes(Paths.get("../preprocessing/data/" + file.getName())));
+                    String content = new String(Files.readAllBytes(Paths.get("../../preprocessing/data/" + file.getName())));
                     StringTokenizer st = new StringTokenizer(content);
                     tokenArray.add(st);
                 } catch (IOException e) {
@@ -50,6 +52,10 @@ public class InvertedIndex {
         for(StringTokenizer st : tokenArray) {
             while(st.hasMoreTokens()) {
                 String temp = st.nextToken();
+                temp = Normalizer.normalize(temp);
+                if (temp == null) {
+                    continue;
+                }
                 System.out.println(temp);
                 dictionary.put(temp, null);
             }
