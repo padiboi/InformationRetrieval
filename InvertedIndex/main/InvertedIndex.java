40c3e9a91479710390d6cfb8b43776f48afcee7c
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-01-15 18:46:45 +0530

    Optimise imports and improve formatting

---
+++ InvertedIndex/main/InvertedIndex.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+import java.io.IOException;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+/**
+ *
+ * @author gautam
+ */
+public class InvertedIndex {
+    
+    TreeMap dictionary = new TreeMap();
+    TreeMap docs = new TreeMap();
+    ArrayList<StringTokenizer> tokenArray = new ArrayList();
+    public InvertedIndex() {
+        constructDictionary(this.dictionary);
+    }
+    
+    public static void main(String[] args) {
+        new InvertedIndex();
+    }              
+    
+    private void constructDictionary(TreeMap dict) {
+        File folder = new File("../preprocessing/data/");
+        File[] listOfFiles = folder.listFiles();
+        
+        int i = 0;
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                docs.put(file.getName(), i++);
+                System.out.println(file.getName());
+                // tokenize and add to dict
+                try {
+                    String content = new String(Files.readAllBytes(Paths.get("../preprocessing/data/" + file.getName())));
+                    StringTokenizer st = new StringTokenizer(content);
+                    tokenArray.add(st);
+                } catch (IOException e) {
+                    System.out.println("Sorry, an error occurred.");
+                }
+            }
+        }
+        
+        for(StringTokenizer st : tokenArray) {
+            while(st.hasMoreTokens()) {
+                String temp = st.nextToken();
+                System.out.println(temp);
+                dictionary.put(temp, null);
+            }
+        }
+    }
+}
